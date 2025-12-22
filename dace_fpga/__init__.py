04647c5637a3a5db763e4361c18ce35376c8effa
Tal Ben-Nun <tbennun@gmail.com>
2025-12-22 08:35:59 -0800

    Implement library entry point

--- dace_fpga/__init__.py
+++ dace_fpga/__init__.py
@@ -1,1 +1,35 @@
 # Copyright 2019-2026 ETH Zurich and the DaCe authors. All rights reserved.
+import os
+
+from dace.config import Config
+from dace import dtypes
+from dace.codegen import dispatcher
+from dace.codegen.tools.type_inference import KNOWN_FUNCTIONS
+
+# Extend the configuration schema with FPGA-specific settings
+Config.extend(os.path.join(os.path.dirname(__file__), 'fpga_config_schema.yml'))
+
+# Extend DaCe data types with FPGA-specific types
+dtypes.ScheduleType.register('FPGA_Multi_Pumped')  #: Used for double pumping
+dtypes.SCOPEDEFAULT_SCHEDULE[dtypes.ScheduleType.FPGA_Multi_Pumped] = dtypes.ScheduleType.FPGA_Device
+
+dtypes.StorageType.register('FPGA_ShiftRegister')  #: Only accessible at constant indices
+dtypes.FPGA_STORAGES.append(dtypes.StorageType.FPGA_ShiftRegister)
+
+dtypes.ScheduleType.register('Unrolled')
+dtypes.SCOPEDEFAULT_SCHEDULE[dtypes.ScheduleType.Unrolled] = dtypes.ScheduleType.CPU_Multicore
+
+# Register FPGA code generation targets and DefinedTypes
+dispatcher.DefinedType.register('FPGA_ShiftRegister')  # A shift-register object used in FPGA code generation
+from dace_fpga.codegen import intel_fpga, rtl, unroller, xilinx
+
+# Register FPGA-specific transformations and passes
+from dace_fpga import transformations
+
+# Register library node expansions
+from dace_fpga import library_nodes
+
+# Register type inference functions
+
+# Reading from an Intel FPGA channel returns the channel type
+KNOWN_FUNCTIONS['read_channel_intel'] = lambda arg_types: arg_types[0]
